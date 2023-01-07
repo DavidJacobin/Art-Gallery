f5679676f1b705fc3a6934b0fa5fead94cc8edbb
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-01-07 10:55:35 +0200

    adding paths and login controller

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 homeController = require('./controllers/homeController');
+loginController = require('./controllers/loginController');
 
 router.use(homeController);
+router.use(loginController);
 
 
 module.exports = router;
