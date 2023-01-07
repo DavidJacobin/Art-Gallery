09a4003a6ae5f8464f43f91df1f698dcbce105b8
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-01-07 09:57:44 +0200

    setting up view engine

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+
+
+const app = express();
+
+app.engine('hbs', hbs.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
+
+app.listen(3000, () => console.log('Server is listening!'));
