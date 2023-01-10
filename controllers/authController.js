2ce8567f0b488932659406f39ed2750889fa6b8a
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-01-10 19:30:26 +0200

    added login and generated jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,12 +5,18 @@ router.get('/login', (req,res) => {
     res.render('auth/login');
 });
 
+router.post('/login', async(req, res) => {
+    const {username, password} = req.body;
+
+    const token = await authService.login(username, password);
+});
+
 router.get('/register', (req,res) => {
     res.render('auth/register');
 });
 
 router.post('/register', async (req,res) => {
-    const {username, password, repassword} = req.body;
+    const {username, password, repassword, address} = req.body;
 
     if(password !== repassword){
         return res.render('auth/register', {error: "Passwords do not match!"})
@@ -18,7 +24,7 @@ router.post('/register', async (req,res) => {
 
     try {
         
-        const createUser = await authService.create({username,password});
+        const createUser = await authService.create({username,password,address});
         res.redirect('/login')
     } catch (error) {
         return res.render('auth/register', {error})
--- services/authServeice.js
+++ services/authServeice.js
@@ -1,3 +1,35 @@
 const User = require('../models/User');
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 exports.create = (userData) => User.create(userData);
+
+exports.login = async (user, password) => {
+    const username = await User.findOne(user);
+
+    if(!user){
+        throw {message: 'Can not find username or password!'}
+    }
+
+    const isvalid = bcrypt.compare(password, user.password)
+
+    if(!isvalid){
+        throw {message: 'Can not find username or password!'} 
+    }
+
+
+    const payload = {_id: user._id, username: user.username, address: user.address}
+
+    const promise = new Promise((resolve, reject) =>{
+        jwt.sign(payload, 'g2734t3eghe8293e', {expiresIn: '2d'}, (err, decToken) =>{
+            if(err){
+                return reject(err);
+            }
+            resolve(decToken); 
+        })
+
+    })
+
+    return promise;
+};
