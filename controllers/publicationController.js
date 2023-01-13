32b75ef6d96a600db36d16a8b61f95791445f84b
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-01-13 11:42:58 +0200

    adding create page with connection to DB

---
+++ controllers/publicationController.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const {isAuth} = require('../middlewares/authMiddleware');
+const publicationService = require('../services/publicationService');
+
+
+router.get('/create',isAuth , (req , res) =>{
+    res.render('publication/create')
+});
+
+router.post('/create',isAuth ,async (req , res) =>{
+    const createPublication = await publicationService.create({...req.body, author: req.user._id});
+
+    res.redirect('/')
+});
+
+
+module.exports = router;
--- routes.js
+++ routes.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 homeController = require('./controllers/homeController');
 authController = require('./controllers/authController');
+createController = require('./controllers/publicationController');
 
 router.use(homeController);
 router.use(authController);
+router.use(createController);
+
 
 
 module.exports = router;
