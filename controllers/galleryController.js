0fd264db2812b88b79b6167de213d052e6104485
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-01-15 11:07:45 +0200

    added publication rendering from DB to site

--- controllers/galleryController.js
+++ controllers/galleryController.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-
+const publicationService = require('../services/publicationService')
 const { isAuth} = require('../middlewares/authMiddleware')
 
-router.get('/gallery',isAuth, (req,res) => {
-    res.render('publications/gallery')
+router.get('/gallery', async (req,res) => {
+    const publications = await publicationService.getAll().lean()
+
+    res.render('publications/gallery', {publications})
 });
 
 
